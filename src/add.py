72c9044cc6250a59be449a95e966a1edd8a935a2
Krishna Kadiyala <kkrishi@gmail.com>
2022-05-12 20:20:01 -0400

    some message

---
+++ src/add.py
@@ -0,0 +1,12 @@
+import numpy as np
+def add(x:np.array, y:np.array) -> np.array:
+    """
+    Add two numbers together.
+
+    Args:
+        x (int): The first number.
+        y (int): The second number. Defaults to 100.
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return x + y
--- tests/test_add.py
+++ tests/test_add.py
@@ -1,3 +1,6 @@
+import numpy as np
 from src.add import add
 def test_add():
-    assert add(1) == 101
+    assert add(np.array([1]),np.array([1])) == [2]
+
+
